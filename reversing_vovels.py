e7294aaab865ba70a4d42bfafd6525878d636a1b
Advaith Krishna <advaithnikunjam@gmail.com>
2025-03-09 21:37:12 +0530

    Update reversing_vovels.py

--- reversing_vovels.py
+++ reversing_vovels.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def reverseVowels(self, s):
-        """
-        :type s: strv
-        :rtype: str
-        """
         vowels = 'aeiouAEIOU'
         s = list(s)
         first,last = 0,len(s)-1
-
         while first<last:
             if s[first] not in vowels:
                 first+=1
@@ -17,9 +12,7 @@ class Solution(object):
                 s[first] , s[last] = s[last], s[first]
                 first = first+1
                 last = last-1
-
         return ''.join(s)
-    
 solution = Solution()
 print(solution.reverseVowels("IceCreAm"))
 print(solution.reverseVowels("leetcode"))
